19ab567241346ea849f088b010c8cafd7e019a64
GaneshSAcharya <31121986g@gmail.com>
2019-02-04 21:01:19 +0530

    everyday learnig dojo

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log(dojo.byId("greeting").innerHTML);
+require([
+
+    'demo/mymodule',
+    'dojo/fx',
+    'dojo/domReady!'
+
+], function (Ganesh, fx) {
+    
+    Ganesh.setText('greeting', 'Hello Dojo!');
+
+    fx.slideTo({
+        node: greeting,
+        top: 100,
+        left: 200
+    }).play();
+
+});
